5be305cb0d45bfaceff45374a735f1362d4735d9
jjk286 <113148937+jjk286@users.noreply.github.com>
2022-10-06 18:34:34 -0400

    buil1.8
    Add collectible coins to our game and count them in the UI.

--- Assets/03-Prototype1/Scripts/Rotate.cs
+++ Assets/03-Prototype1/Scripts/Rotate.cs
@@ -9,14 +9,10 @@ public class Rotate : MonoBehaviour
     [SerializeField] float speedZ;
 
     
-    void Start()
-    {
-        
-    }
 
     
     void Update()
     {
-        
+        transform.Rotate(360 * speedX * Time.deltaTime, 360 * speedY * Time.deltaTime, 360 * speedZ * Time.deltaTime);
     }
 }
